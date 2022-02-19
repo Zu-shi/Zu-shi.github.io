34eda956519eaff81305760f09e2b78d7bb2144c
Zuoming Shi <zuomingshi@gmail.com>
2022-02-19 14:40:59 -0800

    Set minimum width and height for window, make text work correctly when resizing window

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Todo: have a global that gets the x, y, width, height of preview box at any moment
-var Events = [{"Name":"Ground of Being","X":8258,"Y":2249,"Radius":1450},{"Name":"Wings in Self-Strum","X":4666,"Y":2614,"Radius":1450},{"Name":"Wonder Cloud","X":7747,"Y":2825,"Radius":940},{"Name":"Conversation of Quanta","X":959,"Y":3056,"Radius":940},{"Name":"Burst of Solar Structure","X":12199,"Y":2124,"Radius":940},{"Name":"Far Off Emergence","X":1069,"Y":1393,"Radius":940},{"Name":"Present Opalessence","X":11261,"Y":1402,"Radius":940},{"Name":"Architecture","X":4303,"Y":2268,"Radius":940},{"Name":"unfathomable quantum","X":10602,"Y":4411,"Radius":940},{"Name":"Boundless Being","X":8482,"Y":3225,"Radius":940},{"Name":"Layer and Structure","X":3547,"Y":3580,"Radius":940},{"Name":"Here and Now","X":2564,"Y":1857,"Radius":940},{"Name":"Sub Surface Covergence","X":2259,"Y":956,"Radius":940},{"Name":"Inbreath","X":11715,"Y":982,"Radius":940},{"Name":"Awareness Machine","X":10015,"Y":1736,"Radius":940},{"Name":"Feminine Aspect","X":10515,"Y":1700,"Radius":940},{"Name":"Convergence of Spirit","X":8441,"Y":2317,"Radius":940},{"Name":"Preceeding Quanta","X":15399,"Y":958,"Radius":940},{"Name":"Outbreath","X":13543,"Y":1056,"Radius":940},{"Name":"Waves of Ether Into Base Quanta","X":14002,"Y":3433,"Radius":940},{"Name":"Proof of Sun","X":7595,"Y":1945,"Radius":550},{"Name":"Convergence Engine","X":11323,"Y":2046,"Radius":550},{"Name":"Seed of Intention","X":9476,"Y":2662,"Radius":550},{"Name":"Livid Life","X":7904,"Y":757,"Radius":550},{"Name":"Dance of Duality","X":8628,"Y":1344,"Radius":550},{"Name":"Season of Artificial Intelegence","X":7445,"Y":3397,"Radius":550},{"Name":"Life of Leaf","X":6844,"Y":3444,"Radius":550},{"Name":"open hear","X":10816,"Y":3338,"Radius":550},{"Name":"Wave Form","X":10295,"Y":1956,"Radius":550},{"Name":"Helic of Intertwining Self and Other","X":6499,"Y":2547,"Radius":550},{"Name":"Echos of Conversation","X":6242,"Y":2560,"Radius":550},{"Name":"Friction Formula","X":968,"Y":3383,"Radius":550},{"Name":"Fading Architecture, Entropy Affecter","X":4092,"Y":602,"Radius":550},{"Name":"Life Line","X":7577,"Y":3715,"Radius":550},{"Name":"Wisps of Sight","X":2364,"Y":3636,"Radius":550},{"Name":"Duality Bites","X":10617,"Y":2196,"Radius":550},{"Name":"Dimensional Breach","X":6160,"Y":1388,"Radius":550},{"Name":"Horn of Musical Matters","X":10202,"Y":2630,"Radius":550},{"Name":"cloud form","X":5050,"Y":1156,"Radius":550},{"Name":"The floop of fwee","X":5641,"Y":3975,"Radius":550},{"Name":"Lunminous Wing","X":6425,"Y":1785,"Radius":550},{"Name":"Boundless Being","X":8262,"Y":3375,"Radius":550},{"Name":"Song of the Soul","X":8223,"Y":1737,"Radius":550},{"Name":"Diathanous Games with Cumulous Intent","X":11051,"Y":2246,"Radius":550},{"Name":"Airfoil","X":6478,"Y":2004,"Radius":550},{"Name":"Sense Alchemy","X":9185,"Y":1533,"Radius":550},{"Name":"Sleek Curiosity","X":8644,"Y":3121,"Radius":550},{"Name":"pretend occurrence","X":9855,"Y":1530,"Radius":550},{"Name":"Draft point","X":6473,"Y":2219,"Radius":550},{"Name":"Stratified Concious Side","X":8777,"Y":1141,"Radius":550},{"Name":"Gestures of rhythm","X":6353,"Y":2843,"Radius":550},{"Name":"Flutter Form","X":4366,"Y":2595,"Radius":550},{"Name":"Felling Fumes","X":9660,"Y":1935,"Radius":550},{"Name":"Vertext convergence Context Emergence","X":6719,"Y":2223,"Radius":550},{"Name":"Melodic Action","X":6737,"Y":1417,"Radius":550},{"Name":"wedges of transience","X":7058,"Y":2468,"Radius":550},{"Name":"Wafts of Vision","X":558,"Y":3893,"Radius":550},{"Name":"Twinkling \"I","X":6089,"Y":1679,"Radius":275},{"Name":"Wriggle of Spirit","X":8653,"Y":1823,"Radius":275},{"Name":"convergence Engine","X":11019,"Y":1870,"Radius":275},{"Name":"Artifacts of spirit","X":4966,"Y":2718,"Radius":275},{"Name":"The Grey Black and White Agree on Grey","X":6134,"Y":2414,"Radius":275},{"Name":"Frequent - See","X":9542,"Y":2686,"Radius":275},{"Name":"Motion-Forge Radical","X":5776,"Y":2296,"Radius":275},{"Name":"Perk of Will","X":9312,"Y":2484,"Radius":275},{"Name":"illusive bridge","X":5756,"Y":1588,"Radius":275},{"Name":"occam's razor","X":5144,"Y":2576,"Radius":275},{"Name":"The Floop Of Fwee","X":5970,"Y":4038,"Radius":275},{"Name":"Support Agent","X":7069,"Y":3733,"Radius":275},{"Name":"The Shadow Self","X":9940,"Y":2612,"Radius":275},{"Name":"The Point","X":8414,"Y":300,"Radius":275},{"Name":"Elemental Influx","X":9454,"Y":1373,"Radius":275},{"Name":"High Octane Experience","X":6644,"Y":1144,"Radius":275},{"Name":"Shine Refinery","X":8547,"Y":2832,"Radius":275},{"Name":"Wink By Gleaming Glint","X":9765,"Y":2094,"Radius":275},{"Name":"Cubic Kiss","X":5009,"Y":2902,"Radius":275},{"Name":"propagation artifact","X":9447,"Y":1685,"Radius":275},{"Name":"Qauntum attraction","X":1055,"Y":3820,"Radius":275},{"Name":"Tree of Life","X":7112,"Y":2264,"Radius":275},{"Name":"Rhythm of Self Assembly","X":6335,"Y":3511,"Radius":275},{"Name":"Form Curl to Curl","X":1409,"Y":3722,"Radius":275},{"Name":"love Language","X":8867,"Y":920,"Radius":275},{"Name":"percussive lifeforce","X":7883,"Y":1407,"Radius":275},{"Name":"Charming Veneer","X":5106,"Y":2193,"Radius":275},{"Name":"Space Thread","X":7433,"Y":1395,"Radius":275}]
+var Events = [{"Name":"The Grey Black and White Agree on Grey","X":6134,"Y":2414,"Radius":275},{"Name":"Ground of Being","X":8258,"Y":2249,"Radius":1450},{"Name":"Wings in Self-Strum","X":4666,"Y":2614,"Radius":1450},{"Name":"Wonder Cloud","X":7747,"Y":2825,"Radius":940},{"Name":"Conversation of Quanta","X":959,"Y":3056,"Radius":940},{"Name":"Burst of Solar Structure","X":12199,"Y":2124,"Radius":940},{"Name":"Far Off Emergence","X":1069,"Y":1393,"Radius":940},{"Name":"Present Opalessence","X":11261,"Y":1402,"Radius":940},{"Name":"Architecture","X":4303,"Y":2268,"Radius":940},{"Name":"unfathomable quantum","X":10602,"Y":4411,"Radius":940},{"Name":"Boundless Being","X":8482,"Y":3225,"Radius":940},{"Name":"Layer and Structure","X":3547,"Y":3580,"Radius":940},{"Name":"Here and Now","X":2564,"Y":1857,"Radius":940},{"Name":"Sub Surface Covergence","X":2259,"Y":956,"Radius":940},{"Name":"Inbreath","X":11715,"Y":982,"Radius":940},{"Name":"Awareness Machine","X":10015,"Y":1736,"Radius":940},{"Name":"Feminine Aspect","X":10515,"Y":1700,"Radius":940},{"Name":"Convergence of Spirit","X":8441,"Y":2317,"Radius":940},{"Name":"Preceeding Quanta","X":15399,"Y":958,"Radius":940},{"Name":"Outbreath","X":13543,"Y":1056,"Radius":940},{"Name":"Waves of Ether Into Base Quanta","X":14002,"Y":3433,"Radius":940},{"Name":"Proof of Sun","X":7595,"Y":1945,"Radius":550},{"Name":"Convergence Engine","X":11323,"Y":2046,"Radius":550},{"Name":"Seed of Intention","X":9476,"Y":2662,"Radius":550},{"Name":"Livid Life","X":7904,"Y":757,"Radius":550},{"Name":"Dance of Duality","X":8628,"Y":1344,"Radius":550},{"Name":"Season of Artificial Intelegence","X":7445,"Y":3397,"Radius":550},{"Name":"Life of Leaf","X":6844,"Y":3444,"Radius":550},{"Name":"open hear","X":10816,"Y":3338,"Radius":550},{"Name":"Wave Form","X":10295,"Y":1956,"Radius":550},{"Name":"Helic of Intertwining Self and Other","X":6499,"Y":2547,"Radius":550},{"Name":"Echos of Conversation","X":6242,"Y":2560,"Radius":550},{"Name":"Friction Formula","X":968,"Y":3383,"Radius":550},{"Name":"Fading Architecture, Entropy Affecter","X":4092,"Y":602,"Radius":550},{"Name":"Life Line","X":7577,"Y":3715,"Radius":550},{"Name":"Wisps of Sight","X":2364,"Y":3636,"Radius":550},{"Name":"Duality Bites","X":10617,"Y":2196,"Radius":550},{"Name":"Dimensional Breach","X":6160,"Y":1388,"Radius":550},{"Name":"Horn of Musical Matters","X":10202,"Y":2630,"Radius":550},{"Name":"cloud form","X":5050,"Y":1156,"Radius":550},{"Name":"The floop of fwee","X":5641,"Y":3975,"Radius":550},{"Name":"Lunminous Wing","X":6425,"Y":1785,"Radius":550},{"Name":"Boundless Being","X":8262,"Y":3375,"Radius":550},{"Name":"Song of the Soul","X":8223,"Y":1737,"Radius":550},{"Name":"Diathanous Games with Cumulous Intent","X":11051,"Y":2246,"Radius":550},{"Name":"Airfoil","X":6478,"Y":2004,"Radius":550},{"Name":"Sense Alchemy","X":9185,"Y":1533,"Radius":550},{"Name":"Sleek Curiosity","X":8644,"Y":3121,"Radius":550},{"Name":"pretend occurrence","X":9855,"Y":1530,"Radius":550},{"Name":"Draft point","X":6473,"Y":2219,"Radius":550},{"Name":"Stratified Concious Side","X":8777,"Y":1141,"Radius":550},{"Name":"Gestures of rhythm","X":6353,"Y":2843,"Radius":550},{"Name":"Flutter Form","X":4366,"Y":2595,"Radius":550},{"Name":"Felling Fumes","X":9660,"Y":1935,"Radius":550},{"Name":"Vertext convergence Context Emergence","X":6719,"Y":2223,"Radius":550},{"Name":"Melodic Action","X":6737,"Y":1417,"Radius":550},{"Name":"wedges of transience","X":7058,"Y":2468,"Radius":550},{"Name":"Wafts of Vision","X":558,"Y":3893,"Radius":550},{"Name":"Twinkling \"I","X":6089,"Y":1679,"Radius":275},{"Name":"Wriggle of Spirit","X":8653,"Y":1823,"Radius":275},{"Name":"convergence Engine","X":11019,"Y":1870,"Radius":275},{"Name":"Artifacts of spirit","X":4966,"Y":2718,"Radius":275},{"Name":"Frequent - See","X":9542,"Y":2686,"Radius":275},{"Name":"Motion-Forge Radical","X":5776,"Y":2296,"Radius":275},{"Name":"Perk of Will","X":9312,"Y":2484,"Radius":275},{"Name":"illusive bridge","X":5756,"Y":1588,"Radius":275},{"Name":"occam's razor","X":5144,"Y":2576,"Radius":275},{"Name":"The Floop Of Fwee","X":5970,"Y":4038,"Radius":275},{"Name":"Support Agent","X":7069,"Y":3733,"Radius":275},{"Name":"The Shadow Self","X":9940,"Y":2612,"Radius":275},{"Name":"The Point","X":8414,"Y":300,"Radius":275},{"Name":"Elemental Influx","X":9454,"Y":1373,"Radius":275},{"Name":"High Octane Experience","X":6644,"Y":1144,"Radius":275},{"Name":"Shine Refinery","X":8547,"Y":2832,"Radius":275},{"Name":"Wink By Gleaming Glint","X":9765,"Y":2094,"Radius":275},{"Name":"Cubic Kiss","X":5009,"Y":2902,"Radius":275},{"Name":"propagation artifact","X":9447,"Y":1685,"Radius":275},{"Name":"Qauntum attraction","X":1055,"Y":3820,"Radius":275},{"Name":"Tree of Life","X":7112,"Y":2264,"Radius":275},{"Name":"Rhythm of Self Assembly","X":6335,"Y":3511,"Radius":275},{"Name":"Form Curl to Curl","X":1409,"Y":3722,"Radius":275},{"Name":"love Language","X":8867,"Y":920,"Radius":275},{"Name":"percussive lifeforce","X":7883,"Y":1407,"Radius":275},{"Name":"Charming Veneer","X":5106,"Y":2193,"Radius":275},{"Name":"Space Thread","X":7433,"Y":1395,"Radius":275}]
 console.log(Events);
 
 // box = 1374x334
@@ -36,11 +36,17 @@ decorative.onload = function(){
 }
 decorative.src = 'Assets\\CCC_Decorative_Elements.webp';
 
+var scrollbackground = new Image();
+scrollbackground.onload = function(){
+	RedrawCanvas();
+}
+scrollbackground.src = 'Assets\\Brackground for Scroll List.webp';
+
 var looking_glass = new Image();
 looking_glass.onload = function(){
 	RedrawCanvas();
 }
-looking_glass.src = 'Assets\\EEE_Looking_Glass.webp';
+looking_glass.src = 'Assets\\EEE_Looking_Glass_No_Vert_Bar.webp';
 
 var unity = new Image();
 unity.onload = function(){
@@ -61,6 +67,9 @@ const circle_center_y = 1200
 const circle_radius = 420
 const unity_width = 16295
 const unity_height = 4000
+
+const min_width = 1280
+const min_height = 720
 
 function DrawImage(ctx, img, x, y, width, height)
 {
@@ -98,11 +107,18 @@ function GetMainRegion(c)
 {
 	var ctx = c.getContext("2d");
 
-	bodyHeight = c.height;
+	bodyHeight = Math.max(c.height, min_height);
 	bodyWidth = c.width;
 
 	var bodyRatio = bodyWidth / bodyHeight;
 	console.log(bodyWidth, bodyHeight);
+
+
+	// We need to create a scrollbar to use
+	// In the case that width is smaller than implied by height, then the width is equal to ratio to height.
+	// In the case that the width is higher, then a scrollbar is not needed.
+	$("#scrollbox").width(bodyHeight * bodyRatio).height(bodyHeight);
+	$("#scrollbox").width(1080).height(720);
 
 	// Center the body horizontally by calculating the width of the image
 	var new_overall_width = bodyHeight / overall_height * overall_width;
@@ -178,6 +194,7 @@ function RedrawBackground(c)
 	//ctx.drawImage(web, r.x, r.y, r.width, r.height);
 	ctx.drawImage(decorative, r.x, r.y, r.width, r.height);
 	ctx.drawImage(looking_glass, r.x, r.y, r.width, r.height);
+	ctx.drawImage(scrollbackground, r.x, r.y, r.width, r.height);
 
 	ctx.globalAlpha = 0.5;
 	ctx.fillStyle = "#FF0000";
@@ -190,9 +207,6 @@ function RedrawBackground(c)
 	ctx.strokeStyle = '#FFFFFF'
 	ctx.lineWidth = 2
 
-	const font_size = 20;
-	const font_gap = 10;
-	const font_start = 30;
 	startxy = GetMainRegion(c);
 
 	for (let i in Events) {
@@ -205,15 +219,52 @@ function RedrawBackground(c)
 	ctx.globalAlpha = 1;
 
 
-	startx = startxy.x + font_start;
-	starty = startxy.y + font_start;
+	// Need to take into account the StartX area.
+
+	/*
+	const font_size = 13;
+	const font_gap = 10;
+	const font_startx = 90;
+	const font_starty = 30;
+
+	startx = startxy.x + font_startx;
+	starty = startxy.y + font_starty;
+
+	// Full image: 3840
+	// No padding distance to vertical bar: 562
+	// Body width: 1051
+
+	bodyHeight = c.height;
+	bodyWidth = c.width;
+	const full_body_width = 3840
+	const no_padding_vertical_bar_width = 562
+	const experimental_width = 1050
+	const experimental_font_size = 13
+	const no_padding_ratio = no_padding_vertical_bar_width / full_body_width
+	console.log("no_padding_ratio:" + no_padding_ratio)
+	const experimental_font_ratio = experimental_font_size / (experimental_width * no_padding_ratio - font_startx)
+	console.log("experimental_font_ratio:" + experimental_font_ratio)
+	const text_size_to_body_ratio = (bodyWidth * no_padding_ratio - font_startx) * experimental_font_ratio
+	console.log("text_size_to_body_ratio:" + text_size_to_body_ratio)
+	*/
+	
+
+	// Shadow starts at 270
+	// Let font size remain the same
+	
+	const full_start_location_x = 340
+	starty = startxy.y + 30;
+	font_size = 16;
+	starting_location_x = full_start_location_x / overall_width * startxy.width + startxy.x; 
 
 	ctx.globalAlpha = 0.7;
-	for (let i in Events) {
-		ctx.font = "lighter 20px Times New Roman";
+	for (let i in Events)
+	{
+		//ctx.font = "lighter " + text_size_to_body_ratio + "px Times New Roman";ctx.font = "lighter " + text_size_to_body_ratio + "px Times New Roman";
+		ctx.font = "lighter " + font_size + "px Times New Roman";
 		ctx.fillStyle = "#eeeeee";
-		ctx.fillText(Events[i]["Name"], startx, starty);
-		starty += (font_size + font_gap);
+		ctx.fillText(Events[i]["Name"], starting_location_x, starty);
+		starty += (font_size * 2);
 	}
 	ctx.globalAlpha = 1;
 }
@@ -257,7 +308,7 @@ function getUnityPosition(c, x, y)
 
 	// I also need current x, y, in box coords, which I can use to line up.
 	var box = GetNormalizedsFromBoxCoords(c, x, y);
-	console.log(box.x, box.y);
+	//console.log(box.x, box.y);
 
 	// First of all I want to shift image by boxcoords * unity_width, same for height
 	// Then I want to do the half shift to center it. 
@@ -311,8 +362,8 @@ function RedrawCanvas()
 	bodyHeight = innerHeight;
 	bodyWidth = innerWidth;
 	var bodyRatio = bodyWidth / bodyHeight;
-	c.width = window.innerWidth;
-	c.height = window.innerHeight;
+	c.width = Math.max(window.innerWidth, min_width)
+	c.height = Math.max(window.innerHeight, min_height)
 
 	var ctx = c.getContext("2d");
 	ctx.clearRect(0, 0, c.width, c.height);
