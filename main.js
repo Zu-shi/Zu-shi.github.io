d416c5f5c4087f52b9c378fa2b79559c3f969cc5
Zuoming Shi <zuomingshi@gmail.com>
2022-03-08 19:22:55 -0800

    Add working scrollbar

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Todo: have a global that gets the x, y, width, height of preview box at any moment
 var Events = [{"Name":"The Grey Black and White Agree on Grey","X":6134,"Y":2414,"Radius":275},{"Name":"Ground of Being","X":8258,"Y":2249,"Radius":1450},{"Name":"Wings in Self-Strum","X":4666,"Y":2614,"Radius":1450},{"Name":"Wonder Cloud","X":7747,"Y":2825,"Radius":940},{"Name":"Conversation of Quanta","X":959,"Y":3056,"Radius":940},{"Name":"Burst of Solar Structure","X":12199,"Y":2124,"Radius":940},{"Name":"Far Off Emergence","X":1069,"Y":1393,"Radius":940},{"Name":"Present Opalessence","X":11261,"Y":1402,"Radius":940},{"Name":"Architecture","X":4303,"Y":2268,"Radius":940},{"Name":"unfathomable quantum","X":10602,"Y":4411,"Radius":940},{"Name":"Boundless Being","X":8482,"Y":3225,"Radius":940},{"Name":"Layer and Structure","X":3547,"Y":3580,"Radius":940},{"Name":"Here and Now","X":2564,"Y":1857,"Radius":940},{"Name":"Sub Surface Covergence","X":2259,"Y":956,"Radius":940},{"Name":"Inbreath","X":11715,"Y":982,"Radius":940},{"Name":"Awareness Machine","X":10015,"Y":1736,"Radius":940},{"Name":"Feminine Aspect","X":10515,"Y":1700,"Radius":940},{"Name":"Convergence of Spirit","X":8441,"Y":2317,"Radius":940},{"Name":"Preceeding Quanta","X":15399,"Y":958,"Radius":940},{"Name":"Outbreath","X":13543,"Y":1056,"Radius":940},{"Name":"Waves of Ether Into Base Quanta","X":14002,"Y":3433,"Radius":940},{"Name":"Proof of Sun","X":7595,"Y":1945,"Radius":550},{"Name":"Convergence Engine","X":11323,"Y":2046,"Radius":550},{"Name":"Seed of Intention","X":9476,"Y":2662,"Radius":550},{"Name":"Livid Life","X":7904,"Y":757,"Radius":550},{"Name":"Dance of Duality","X":8628,"Y":1344,"Radius":550},{"Name":"Season of Artificial Intelegence","X":7445,"Y":3397,"Radius":550},{"Name":"Life of Leaf","X":6844,"Y":3444,"Radius":550},{"Name":"open hear","X":10816,"Y":3338,"Radius":550},{"Name":"Wave Form","X":10295,"Y":1956,"Radius":550},{"Name":"Helic of Intertwining Self and Other","X":6499,"Y":2547,"Radius":550},{"Name":"Echos of Conversation","X":6242,"Y":2560,"Radius":550},{"Name":"Friction Formula","X":968,"Y":3383,"Radius":550},{"Name":"Fading Architecture, Entropy Affecter","X":4092,"Y":602,"Radius":550},{"Name":"Life Line","X":7577,"Y":3715,"Radius":550},{"Name":"Wisps of Sight","X":2364,"Y":3636,"Radius":550},{"Name":"Duality Bites","X":10617,"Y":2196,"Radius":550},{"Name":"Dimensional Breach","X":6160,"Y":1388,"Radius":550},{"Name":"Horn of Musical Matters","X":10202,"Y":2630,"Radius":550},{"Name":"cloud form","X":5050,"Y":1156,"Radius":550},{"Name":"The floop of fwee","X":5641,"Y":3975,"Radius":550},{"Name":"Lunminous Wing","X":6425,"Y":1785,"Radius":550},{"Name":"Boundless Being","X":8262,"Y":3375,"Radius":550},{"Name":"Song of the Soul","X":8223,"Y":1737,"Radius":550},{"Name":"Diathanous Games with Cumulous Intent","X":11051,"Y":2246,"Radius":550},{"Name":"Airfoil","X":6478,"Y":2004,"Radius":550},{"Name":"Sense Alchemy","X":9185,"Y":1533,"Radius":550},{"Name":"Sleek Curiosity","X":8644,"Y":3121,"Radius":550},{"Name":"pretend occurrence","X":9855,"Y":1530,"Radius":550},{"Name":"Draft point","X":6473,"Y":2219,"Radius":550},{"Name":"Stratified Concious Side","X":8777,"Y":1141,"Radius":550},{"Name":"Gestures of rhythm","X":6353,"Y":2843,"Radius":550},{"Name":"Flutter Form","X":4366,"Y":2595,"Radius":550},{"Name":"Felling Fumes","X":9660,"Y":1935,"Radius":550},{"Name":"Vertext convergence Context Emergence","X":6719,"Y":2223,"Radius":550},{"Name":"Melodic Action","X":6737,"Y":1417,"Radius":550},{"Name":"wedges of transience","X":7058,"Y":2468,"Radius":550},{"Name":"Wafts of Vision","X":558,"Y":3893,"Radius":550},{"Name":"Twinkling \"I","X":6089,"Y":1679,"Radius":275},{"Name":"Wriggle of Spirit","X":8653,"Y":1823,"Radius":275},{"Name":"convergence Engine","X":11019,"Y":1870,"Radius":275},{"Name":"Artifacts of spirit","X":4966,"Y":2718,"Radius":275},{"Name":"Frequent - See","X":9542,"Y":2686,"Radius":275},{"Name":"Motion-Forge Radical","X":5776,"Y":2296,"Radius":275},{"Name":"Perk of Will","X":9312,"Y":2484,"Radius":275},{"Name":"illusive bridge","X":5756,"Y":1588,"Radius":275},{"Name":"occam's razor","X":5144,"Y":2576,"Radius":275},{"Name":"The Floop Of Fwee","X":5970,"Y":4038,"Radius":275},{"Name":"Support Agent","X":7069,"Y":3733,"Radius":275},{"Name":"The Shadow Self","X":9940,"Y":2612,"Radius":275},{"Name":"The Point","X":8414,"Y":300,"Radius":275},{"Name":"Elemental Influx","X":9454,"Y":1373,"Radius":275},{"Name":"High Octane Experience","X":6644,"Y":1144,"Radius":275},{"Name":"Shine Refinery","X":8547,"Y":2832,"Radius":275},{"Name":"Wink By Gleaming Glint","X":9765,"Y":2094,"Radius":275},{"Name":"Cubic Kiss","X":5009,"Y":2902,"Radius":275},{"Name":"propagation artifact","X":9447,"Y":1685,"Radius":275},{"Name":"Qauntum attraction","X":1055,"Y":3820,"Radius":275},{"Name":"Tree of Life","X":7112,"Y":2264,"Radius":275},{"Name":"Rhythm of Self Assembly","X":6335,"Y":3511,"Radius":275},{"Name":"Form Curl to Curl","X":1409,"Y":3722,"Radius":275},{"Name":"love Language","X":8867,"Y":920,"Radius":275},{"Name":"percussive lifeforce","X":7883,"Y":1407,"Radius":275},{"Name":"Charming Veneer","X":5106,"Y":2193,"Radius":275},{"Name":"Space Thread","X":7433,"Y":1395,"Radius":275}]
+var EventHitboxesNormalized = []
 console.log(Events);
 
 // box = 1374x334
@@ -35,6 +36,12 @@ unity.onload = function(){
 	RedrawCanvas();
 }
 unity.src = 'Assets\\AAA_We_Are_Unity_(WebP 90_).webp';
+
+var scrollBar = new Image();
+scrollBar.onload = function(){
+	RedrawCanvas();
+}
+scrollBar.src = 'Assets\\Scroll List Scroll Bar.webp';
 
 /*
 var eventFont = new FontFace('myFont', 'url(Assets\\GABRIOLA.TTF)');
@@ -124,6 +131,11 @@ var getTextHeight = function(font) {
 function DrawImage(ctx, img, x, y, width, height)
 {
 	ctx.drawImage(img.width, img.height, x, y, width, height)
+}
+
+function DrawImageCentered(ctx, img, x, y, width, height)
+{
+	ctx.drawImage(img, x - width / 2, y - height / 2, width, height)
 }
 
 function DrawCircle(ctx, x, y, radius, color)
@@ -250,6 +262,8 @@ function DrawScrollArea(c)
 	starty = startxy.y + 30;
 	font_size = 20;
 	starting_location_x = full_start_location_x / overall_width * startxy.width + startxy.x;
+
+	EventHitboxes = []
 
 	ctx.globalAlpha = 0.7;
 	for (let i in Events)
@@ -467,7 +481,7 @@ function RedrawBackground(c)
 
 	for (let i in Events) {
 		var circle = GetBoxCoordsFromNormalizeds(c, Events[i]["X"] / unity_width, Events[i]["Y"] / unity_height);
-	  	var radius = GetBoxCoordsFromNormalizeds(c, (Events[i]["X"] + Events[i]["Radius"]) / unity_width, 0).x - GetBoxCoordsFromNormalizeds(c, Events[i]["X"] / unity_width, 0).x;
+	  var radius = GetBoxCoordsFromNormalizeds(c, (Events[i]["X"] + Events[i]["Radius"]) / unity_width, 0).x - GetBoxCoordsFromNormalizeds(c, Events[i]["X"] / unity_width, 0).x;
 		//DrawCircle(ctx, circle.x, circle.y, radius, "#FFFFFF");
 		//DrawCross(ctx, circle.x, circle.y, radius / 2, "#FFFFFF");
 	}
@@ -475,7 +489,64 @@ function RedrawBackground(c)
 	ctx.globalAlpha = 1;
 
 	// DrawScrollArea(c);
-	DrawReorderingArea(c)
+	// DrawReorderingArea(c)
+	DrawScrollingBarArea(c);
+}
+
+function DrawScrollingBarArea(c)
+{
+	DrawBox(c);
+
+	var ctx = c.getContext("2d");
+	startxy = GetMainRegion(c);
+	r = startxy;
+	ctx.drawImage(scrollbackground, r.x, r.y, r.width, r.height);
+
+	// Shadow starts at 340
+	// Let font size remain the same
+	const full_start_location_x = 340
+	starty = startxy.y + 30; // This is where you want the first item to be if you scroll to top.
+	font_size = 30;
+	starting_location_x = full_start_location_x / overall_width * startxy.width + startxy.x;
+
+	var font = "lighter " + font_size + "px Gabriola";
+	ctx.font = font;
+	ctx.fillStyle = "#eeeeee";
+	let font_height = getTextHeight(font).height;
+	let expected_rectangle_height = 30 + (Events.length - 1) * 2 * font_size + font_height + 30;
+
+	ctx.globalAlpha = 1;
+	boxNormalized = (boxY - scrollbar_top_pos_y) / (scrollbar_bottom_pos_y - scrollbar_top_pos_y);
+
+	EventHitboxesNormalized = []
+
+	starty = 50 - (boxNormalized * (expected_rectangle_height - startxy.height));
+	//console.log("BoxNormalized: " + boxNormalized)
+	//console.log("StartY" + starty)
+	for (let i in Events)
+	{
+		ctx.fillText(Events[i]["Name"], starting_location_x, starty);
+		starty += (font_size * 2);
+		EventHitboxesNormalized.push({
+			"x": full_start_location_x / overall_width, 
+			"y": starty / startxy.height, 
+			"width": ctx.measureText(Events[i]["Name"]) / startxy.width, 
+			"height": font_height / startxy.height});
+	}
+
+	ctx.globalAlpha = 1;
+}
+
+function DrawBox(c)
+{
+	var ctx = c.getContext("2d");
+	var r = GetMainRegion(c);
+	DrawImageCentered(ctx, scrollBar,
+		boxX * r.width,
+		boxY * r.height,
+		r.width * box_raw_width / overall_width, 
+		r.height * box_raw_height / overall_height
+	)
 }
 
 let unity_move_speed = 0.035;
@@ -485,7 +556,7 @@ let unity_current_pos_y = 0;
 let unity_target_pos_x = 0;
 let unity_target_pos_y = 0;
 
-
+// Normalized
 let mouse_current_pos_x = 0;
 let mouse_current_pos_y = 0;
 let mouse_target_pos_x = 0;
@@ -592,6 +663,31 @@ function onMouseMove(e)
 	unityPos = getUnityPosition(c, mouse_target_pos_x, mouse_target_pos_y)
 
 	last_mouse_move_time_ms = Date.now();
+
+	if (scrollBoxSelected)
+	{
+		mouse_y = convertHtmlDistanceToNormalizedDistanceY(c, y);
+		dy = mouse_y - scrollBoxSelectedPosition.y;
+		if (dy > 0)
+		{
+			boxY = Math.min(scrollBoxSelectedPosition.y + dy, scrollbar_bottom_pos_y);
+		}
+		else
+		{
+			boxY = Math.max(scrollBoxSelectedPosition.y + dy, scrollbar_top_pos_y);
+		}
+	}
+}
+
+function onMouseDown(e)
+{
+	var c = document.getElementById("canvas");
+	hitScrollBoxTest(c, e);
+}
+
+function onMouseUp(e)
+{
+	scrollBoxSelected = false;
 }
 
 function RedrawCanvas()
@@ -620,6 +716,10 @@ function RedrawCanvas()
 $( document ).ready(function() {
     console.log( "ready!" );
 	document.addEventListener('mousemove', onMouseMove);
+	document.addEventListener('mousedown', onMouseDown);
+	document.addEventListener('mouseclick', onMouseClick);
+	document.addEventListener('wheel', onScroll);
+	document.addEventListener('mouseup', onMouseUp);
     RedrawCanvas();
 	animate();
 });
@@ -628,7 +728,130 @@ function onResize(){
 	RedrawCanvas();
 }
 
-function globalToBoxNormalizedCoords()
-{
+const scrollbar_top_pos_x = 0.1
+const scrollbar_top_pos_y = 2.5 / 16
+const scrollbar_bottom_pos_x = 0.1
+const scrollbar_bottom_pos_y = 13.5 / 16
+const box_raw_width = 63;
+const box_raw_height = 561;
+let boxX = 0.05
+let boxY = 0.5
+let scrollBoxSelected = false;
+let scrollBoxTextSize = 30;
+let scrollBoxSelectedPosition = {"x": 0, "y": 0};
 
+function hitScrollBoxTest(c, mouse_pos_raw)
+{
+	mouse_x = convertHtmlDistanceToNormalizedDistanceX(c, mouse_pos_raw.x)
+	mouse_y = convertHtmlDistanceToNormalizedDistanceY(c, mouse_pos_raw.y)
+	console.log("mouse_pos", mouse_x, mouse_y)
+
+	if (!scrollBoxSelected)
+	{
+		box_left = boxX - convertFullImageDistanceToNormalizedDistanceX(c, box_raw_width) / 2;
+		box_right = box_left + convertFullImageDistanceToNormalizedDistanceX(c, box_raw_width);
+		box_top = boxY - convertFullImageDistanceToNormalizedDistanceY(c, box_raw_height) / 2;
+		box_bottom = box_top + convertFullImageDistanceToNormalizedDistanceY(c, box_raw_height);
+
+		console.log("left, right, top, bottom: ", box_left, box_right, box_top, box_bottom);
+
+		if (mouse_x < box_right && mouse_x > box_left && mouse_y < box_bottom && mouse_y > box_top && !scrollBoxSelected)
+		{
+			console.log("selected");
+			scrollBoxSelected = true;
+			scrollBoxSelectedPosition["x"] = mouse_x;
+			scrollBoxSelectedPosition["y"] = mouse_y;
+		}
+	}
 }
+
+function OnClick(e)
+{
+	var c = document.getElementById("canvas");
+	hitTextTest(c, e);
+}
+
+function hitTextTest(c, mouse_pos_raw)
+{
+	mouse_x = convertHtmlDistanceToNormalizedDistanceX(c, mouse_pos_raw.x)
+	mouse_y = convertHtmlDistanceToNormalizedDistanceY(c, mouse_pos_raw.y)
+	console.log("mouse_pos", mouse_x, mouse_y)
+
+	for (var i = 0; i < Events.length; i++)
+	{
+		box_left = EventHitboxesNormalized[i].x
+		box_right = EventHitboxesNormalized[i].x + EventHitboxesNormalized[i].width
+		box_top = EventHitboxesNormalized[i].y
+		box_bottom = EventHitboxesNormalized[i].y + EventHitboxesNormalized[i].height
+
+		if (mouse_x < box_right && mouse_x > box_left && mouse_y < box_bottom && mouse_y > box_top)
+		{
+				console.log("selected");
+				// Move to Unity Position
+				// scrollBoxSelectedPosition["x"] = mouse_x;
+				// scrollBoxSelectedPosition["y"] = mouse_y;
+		}
+	}
+}
+
+function convertFullImageDistanceToNormalizedDistanceX(c, value)
+{
+	return value / overall_width;
+}
+
+function convertFullImageDistanceToNormalizedDistanceY(c, value)
+{
+	return value / overall_height;
+}
+
+function convertHtmlDistanceToNormalizedDistanceX(c, value)
+{
+	return (value - GetMainRegion(c).x) / GetMainRegion(c).width;
+}
+
+function convertHtmlDistanceToNormalizedDistanceY(c, value)
+{
+	return value / GetMainRegion(c).height;
+}
+
+
+function onScroll(e) {
+  //e.preventDefault();
+
+  //const dx = e.evt.deltaX;
+  const deltaY = e.deltaY;
+
+/*
+  const minX = stage.width() - canvasWidth;
+  const maxX = 0;
+
+  const x = Math.max(minX, Math.min(layer.x() - dx, maxX));
+
+  const minY = stage.height() - canvasHeight;
+  const maxY = 0;
+
+  const y = Math.max(minY, Math.min(layer.y() - dy, maxY));
+  layer.position({ x, y });
+
+  const availableHeight = stage.height() - verticalScrollbar.height();
+  const vy = (layer.y() / (stage.height() - canvasHeight)) * availableHeight;
+  verticalScrollbar.y(vy);
+
+  const availableWidth = stage.width() - horizontalScrollbar.width();
+
+  const hx = (layer.x() / (stage.width() - canvasWidth)) * availableWidth;
+  horizontalScrollbar.x(hx);
+*/
+
+	var c = document.getElementById("canvas");
+	dy = convertHtmlDistanceToNormalizedDistanceY(c, deltaY) / 4;
+	console.log(dy)
+	if (dy > 0)
+	{
+		boxY = Math.min(boxY + dy, scrollbar_bottom_pos_y);
+	}
+	else
+	{
+		boxY = Math.max(boxY + dy, scrollbar_top_pos_y);
+	}
+};
